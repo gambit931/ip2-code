72c2536ebc6d59f5e88a264299cb4359f7f44027
NathanTWIV <NTWITE200@caledonian.ac.uk>
2022-03-30 09:59:20 +0100

    Calendar style change

---
+++ calendar.js
@@ -0,0 +1,19 @@
+const date = new Date();
+const months = [
+"January"
+"February"
+"March"
+"April"
+"May"
+"June"
+"July"
+"August"
+"September"
+"October"
+"November"
+"December"
+];
+
+document.querySelector('.date h1').innerHTML
+
+

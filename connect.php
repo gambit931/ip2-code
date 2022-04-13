4fac67306be2e9f25d8936b7128d5d3d9eb13ee6
yusaf <yusaf_safdar@hotmail.com>
2022-04-13 16:19:59 +0100

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,54 @@
+<?php
+
+ 
+  $firstName = filter_input(INPUT_POST, 'firstName');
+  $password = filter_input(INPUT_POST, 'password');
+  $email = filter_input(INPUT_POST, 'email');
+  $number = filter_input(INPUT_POST, 'number');
+
+
+
+  if (!empty($firstName)) {
+	  if (!empty($password)) {
+		  if (!empty($email)) {
+			  if (!empty($number)) {
+
+			  
+		  
+		  $host = "localhost";
+		  $dbfirstName = "root";
+		  $dbpassword = "";
+		  $dbemail = "";
+		  $dbnumber = "";
+		  $dbname = "ip2database";
+
+
+		  $conn = new mysqli ($host, $dbfirstName, $dbpassword, $dbemail, $dbnumber, $dbname);
+
+			  }
+			}
+		}
+	}
+
+
+		  if (mysqli_connect_error()) {
+			  die('Connect Error ( '.
+			  					mysqli_connect_errno().')' . 
+								mysqli_connect_error());
+
+		  }
+		  else {
+			  $sql = "INSERT INTO form(firstName, password, email, number) values ('$firstName', '$password', '$email', '$number')";
+			  if ($conn->query($sql)) {
+				  echo "New record is inserted successfully";
+			  }
+			  else {
+				  echo "Error: ". $sql . "<br>". $conn->error;
+			  }
+
+			  }
+			  $conn->close();
+	
+
+
+ ?>

4c8544aee2c232d7a7121b794a069a58a52d9339
NathanTWIV <NTWITE200@caledonian.ac.uk>
2022-04-18 20:27:03 +0100

    Register style change

--- register.php
+++ register.php
@@ -1,30 +1,47 @@
 <?php
+$userName = filter_input(INPUT_POST, 'userName');
+$password = filter_input(INPUT_POST, 'password');
+$email = filter_input(INPUT_POST, 'email');
+$number = filter_input(INPUT_POST, 'number');
+$messageBox = filter_input(INPUT_POST, 'messageBox');
 
-if(isset($_POST['userName']))
+if (!empty($username)){
+if (!empty($password)){
+if (!empty($email)){
+if (!empty($number)){
+if (!empty($messageBox)){
 
-$con = mysqli_connect('localhost','root',
-       '','day1');
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "day1";
 
-       $userName = $_POST['userName'];
-       $password = $_POST['password'];
-       $email = $_POST['email'];
-       $number = $_POST['number'];
-       $messageBox = $_POST['messageBox'];
-
-       $sql = "INSERT INTO `register`
-       (`Username`, `Password`, `Email`, `Number`, `Message Box`, `id`)
-       VALUES ('$userName', '$password', '$email', '$number', '$messageBox', '0')";
-
-$rs = mysqli_query($con, $sql);
-
-      if($rs)
-      {
-      	echo "Register information taken in";
-      }
-
-      else
-      {
-      	echo "Are you a bot?";
-
-      }
-      ?>
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '
+. mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO register (Username, Password, Email, Number, Message Box, id)
+values ('$userName','$password','$email','$number','$messageBox','0')";
+if ($conn->query($sql)){
+echo "New registration has been sucessfully entered";
+}
+else{
+echo "Error: ". $sql ."
+". $conn->error;
+}
+$conn->close();
+}
+}
+else{
+echo "Password should not be empty";
+die();
+}
+}
+else{
+echo "Username should not be empty";
+die();
+}
+?>

4c0f48bf1ed1ec9fb655193ada1923a60b6f4fb4
NathanTWIV <NTWITE200@caledonian.ac.uk>
2022-04-18 12:37:08 +0100

    Register style change

--- connect.php
+++ connect.php
@@ -2,15 +2,18 @@
 
 $con = mysqli_connect("localhost","root",
        "","register");
+
        $userName = $_POST['userName'];
-       $password = $_POST['password']
+       $password = $_POST['password'];
        $email = $_POST['email'];
        $number = $_POST['phone'];
        $messageBox = $_POST['messageBox'];
+
        $sql = "INSERT INTO `register`
        (`Username`, `Password`, `Email`, `Number`, `Message Box`, `id`)
        VALUES ('$userName', '$password', '$email', '$number', '$messageBox', '0');"
        $rs = mysqli_query($con, $sql);
+
        if($rs)
        {
        	echo "Contact Records Inserted";
